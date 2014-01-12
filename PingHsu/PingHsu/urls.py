1559653f2a428018e8a684fbcb99082e63cadc93
squall <squall@squall-PC>
2014-01-12 12:27:26 +0800

    Django enviroment setting and activate the page

--- PingHsu/PingHsu/urls.py
+++ PingHsu/PingHsu/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+import settings
+import os
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,7 +13,12 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+ 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^css/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(settings.PROJECT_PATH, 'static/css')}),
+    url(r'^js/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(settings.PROJECT_PATH, 'static/js')}),
+    url(r'^img/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(settings.PROJECT_PATH, 'static/img')}),
+    url(r'^$', 'PingHsu.views.index'),
 )
